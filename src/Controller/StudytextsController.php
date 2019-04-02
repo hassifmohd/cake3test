5a7ad27691922841724ced8b0fcc542dfc0b39a5
hassifmohd <mohdhassif.jabir@tyrellsys.com.my>
2019-04-03 00:14:09 +0800

    put more study

--- src/Controller/StudytextsController.php
+++ src/Controller/StudytextsController.php
@@ -1,18 +1,34 @@
 <?php
+
 namespace App\Controller;
 
 use Cake\Utility\Text;
 
-class StudytextsController extends AppController
-{
-    public function index()
-    {
+class StudytextsController extends AppController {
+
+    public function index() {
+        
+        //getTransliteratorId
+        if (0) {
+            $result = Text::getTransliteratorId();
+            pr($result);
+        }
+        
         //Transliterate
         if (0) {
             $result = Text::transliterate('apple purée');
             pr($result);
 
             $result = Text::transliterate('apple purée', 'Latin-ASCII;');
+            pr($result);
+            
+            $result = Text::transliterate('Übérmensch');
+            pr($result);
+
+            $result = Text::transliterate('Übérmensch', 'Latin-ASCII;');
+            pr($result);
+
+            $result = Text::transliterate('こんにちは', 'Latin-ASCII;');
             pr($result);
         }
 
@@ -30,12 +46,19 @@ class StudytextsController extends AppController
             ]);
             pr($result);
 
-            //prserve a dot .
+            //preserve a dot .
             $result = Text::slug('apple purée aur.pérfume', [
                 'preserve' => '.',
                 'replacement' => '|',
             ]);
             pr($result);
+
+            //preserve a dot .
+//            $result = Text::slug('apple purée aur.pérfume', [
+//                'preserve' => ['.'], <-- will not work. did not support multiple preserve
+//                'replacement' => '+',
+//            ]);
+//            pr($result);
         }
 
         //Generating UUID
@@ -55,23 +78,152 @@ class StudytextsController extends AppController
             //CSV data eg: table users
             $data = "1,Mr Wong,wong@gmail.com,admin";
             $result = Text::tokenize($data, ',');
-			pr($result);
-			
-			//CSV bracket data eg: articles tags
-			$data = "(cat),(funny),(top-page),(sponsored content)";
-            $result = Text::tokenize($data, ',');
-			pr($result);
-			
-			//CSV bracket data using bound. Without bound 'top' and 'page' will be separated
-			$data = "(cat),(funny),,(top,page),(sponsored,content)";
-            $result = Text::tokenize($data, ',', '(', ')');
-			pr($result);
-		}
-		
-		//Simple String Parsing - parseFileSize
-		if (1) {
-			$result = Text::parseFileSize('2GB');
             pr($result);
-		}
+
+            //CSV bracket data eg: articles tags
+            $data = "(cat),(funny),(top-page),(sponsored content)";
+            $result = Text::tokenize($data, ',');
+            pr($result);
+
+            //CSV bracket data using bound. Without bound 'top' and 'page' will be separated
+            $data = "(cat),(funny),,(top,page),(sponsored,content)";
+            $result = Text::tokenize($data, ',', '(', ')');
+            pr($result);
+        }
+
+        //Simple String Parsing - parseFileSize
+        if (0) {
+            $result = Text::parseFileSize('2GB');
+            pr($result);
+        }
+
+        //insert
+        if (0) {
+            $result = Text::insert(
+                'My name is :name and I am :age years old.',
+                ['name' => 'Bob', 'age' => '65']
+            );
+            pr($result);
+            
+            $result = Text::insert(
+                'My name is |name| and I am |age| years old.',
+                ['name' => 'Rob', 'age' => '40'],
+                ['before' => '|', 'after' => '|'] //extra options
+            );
+            pr($result);
+            
+            //clean insert
+            $options = [
+                'clean' => [
+//                    'method' => 'text',
+                    'method' => 'html',
+                ],
+                'before' => ':',
+                'after' => ''
+            ];
+            
+            $result = Text::insert(
+                'My name is :name and I am :age years old.',
+                ['name' => 'Bob']
+            );
+            pr($result);
+            
+            $result = Text::insert(
+                'My name is :name and I am :age years old.',
+                ['name' => 'Red'],
+                $options
+            );
+            pr($result);
+            
+            $result = Text::cleanInsert('My name is :name and I am :age years old.', $options);
+            pr($result);
+            
+            $options = [
+                'clean' => [
+                    'method' => 'html',
+                ],
+                'before' => ':',
+                'after' => ''
+            ];
+            
+            $result = Text::cleanInsert('My <br/> name is <b>:name</b> and I am :age years old.', $options);
+            pr($result);
+            
+            $result = Text::insert(
+                '<b>Boom :name :age Room</b>',
+                ['name' => 'Ted'],
+                $options
+            );
+            pr($result);
+        }
+        
+        //Wrapping Text
+        //https://book.cakephp.org/3.0/en/core-libraries/text.html#wrapping-text
+        if (1) {
+            $text = 'This is a song that never ends.';
+            
+            $result = Text::wrap($text, 22);
+            pr($result);
+            
+            $result = Text::wrap($text, [
+                'width' => 10
+            ]);
+            pr($result);
+            
+            $result = Text::wrap($text, [
+                'width' => 22,
+                'wordWrap' => false
+            ]);
+            pr($result);
+            
+            $result = Text::wrap($text, [
+                'width' => 22,
+                'wordWrap' => true,
+                'indent' => '+'
+            ]);
+            pr($result);
+            
+            $result = Text::wrap($text, [
+                'width' => 22,
+                'wordWrap' => true,
+                'indent' => '+',
+                'indentAt' => 1
+            ]);
+            pr($result);
+            
+            $result = Text::wrap($text, [
+                'width' => 10,
+                'wordWrap' => true,
+                'indent' => '+',
+                'indentAt' => 2
+            ]);
+            pr($result);
+            
+            $result = Text::wrap($text, [
+                'width' => 10,
+                'wordWrap' => true,
+                'indent' => 'continue... ',
+                'indentAt' => 1
+            ]);
+            pr($result);
+            
+            $result = Text::wrap('2019-03', [
+                'width' => 4,
+                'wordWrap' => '-'
+            ]);
+            pr($result);
+            
+            //some interesting example differences between tokenize & wordWrap
+            $yearMonth = '2017-03';
+//            $yearMonth = '201703';
+//            $yearMonth = '2017Feb';
+//            $yearMonth = '2017April';
+            $result = Text::tokenize($yearMonth, '-');
+            pr($result);
+            
+            $result = Text::wordWrap($yearMonth, 4, '-', true);
+            pr($result);
+        }
     }
+
 }
